82107733493fe551f3c6c0c7d0ff7f7f3be3b23b
zeroone-kr <dnjsdlf61@naver.com>
2023-05-31 20:54:40 +0900

    230531

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "foo.h"
+
+int main(){
+    // std::cout << "hello, cmake" << std::endl;
+    int i;
+    std::cout << "Foo : " << foo() << std::endl;
+    return 0;
+}
