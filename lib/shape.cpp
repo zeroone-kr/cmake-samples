a60126bd935240013fed24ed84288a4577212564
zeroone-kr <dnjsdlf61@naver.com>
2023-06-01 00:25:00 +0900

    pthread library linking using target_link_libraries()

--- lib/shape.cpp
+++ lib/shape.cpp
@@ -1,8 +1,17 @@
+#include <iostream>
+#include <thread>
+
 #include "shape.h"
+
+using namespace std;
 
 Rectangle::Rectangle(int width, int height) : width_(width), height_(height){}
 
 int Rectangle::GetSize() const{
+
+    thread t([this]() { cout << "Calculate .. " << endl;});
+    t.join();
+
     // 직사각형의 넓이를 리턴한다.
     return width_ * height_;
 }
